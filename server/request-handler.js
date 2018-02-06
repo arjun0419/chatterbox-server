05125d506bd5646d432614704612b4d6cbe0c631
arjun0419 <arjunchemparathy@gmail.com>
2018-02-05 21:51:30 -0800

    eod pass tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,49 +12,6 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // The outgoing status.
-  var statusCode = 200;
-
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end('Hello, World!');
-};
-
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
 // are on different domains, for instance, your chat client.
@@ -64,6 +21,10 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
+
+var messageStorage = [];
+var roomStorage = [];
+
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
@@ -71,3 +32,90 @@ var defaultCorsHeaders = {
   'access-control-max-age': 10 // Seconds.
 };
 
+var headers = defaultCorsHeaders;
+headers['Content-Type'] = 'application/json';
+
+var requestHandler = function(request, response) {
+
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+    
+  let {method} = request;
+
+
+  if (method === 'POST' && request.url === '/classes/messages') {
+    
+    let body = [];
+
+    request.on('error', (err) => {
+      console.error(err);
+    }).on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      // BEGINNING OF NEW STUFF
+      messageStorage.push(JSON.parse(body));
+      statusCode = 201;
+
+      response.writeHead(statusCode, headers);
+
+      var data = {};
+      data.results = [];
+      console.log('6 ', statusCode);
+      if (messageStorage.length > 0) {
+        messageStorage.forEach(function(message) {
+          data.results.push(message);
+        });
+      }
+
+      response.end(JSON.stringify(data));
+    });
+
+  } else if (method === 'POST' && request.url === '/classes/room') {
+    
+    let body = [];
+    request.on('error', (err) => {
+      console.error(err);
+    }).on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      // BEGINNING OF NEW STUFF
+      messageStorage.push(JSON.parse(body));
+      statusCode = 201;
+      console.log('2 ', statusCode);
+
+      response.writeHead(statusCode, headers);
+
+      var data = {};
+      data.results = [];
+      if (messageStorage.length > 0) {
+        messageStorage.forEach(function(message) {
+          data.results.push(message);
+        });
+      }
+
+      response.end(JSON.stringify(data));
+    });
+  } else if (method === 'GET' && request.url === '/classes/messages') {
+    statusCode = 200;
+    response.writeHead(statusCode, headers);
+
+    var data = {};
+    data.results = [];
+    if (messageStorage.length > 0) {
+      messageStorage.forEach(function(message) {
+        data.results.push(message);
+      });
+    }
+
+    response.end(JSON.stringify(data));
+
+  } else {
+    statusCode = 404;
+    response.writeHead(statusCode, headers);
+    response.end(JSON.stringify(data));
+  }
+};
+
+exports.requestHandler = requestHandler;
+exports.defaultCorsHeaders = defaultCorsHeaders;
